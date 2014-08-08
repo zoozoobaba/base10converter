1a057b4dc142a577050ad285a1c0be0f1c53e419
Ruben Rhodes <student@epicodus.com>
2014-08-08 11:54:21 -0700

    add a convert_to_dec method

--- lib/converters.rb
+++ lib/converters.rb
@@ -1,7 +1,29 @@
 class Converter
 
   def initialize(number)
-    @number = number
+    @number = number.to_i
+    @converted_array = []
   end
 
+  def number
+    @number
+  end
+
+  def converted_array
+    @converted_array
+  end
+
+  def convert_to_dec
+    converted_array << @number % 2
+    while @number > 1 do
+      converted_array << (@number = number / 2) % 2
+    end
+    @converted_array.reverse!.join("").to_i
+  end
+
+
 end
+
+# new_conversion = Converter.new(6)
+# new_conversion.convert_to_dec
+# p new_conversion.converted_array
--- spec/converters_spec.rb
+++ spec/converters_spec.rb
@@ -8,4 +8,26 @@ describe "Converter" do
       expect(new_conversion).to be_an_instance_of(Converter)
     end
   end
+
+  describe "convert_to_dec" do
+    it "will return 1 for the number 1" do
+      new_conversion = Converter.new(1)
+      expect(new_conversion.convert_to_dec).to eq(1)
+    end
+
+    it "will return 10 for the number 2" do
+      new_conversion = Converter.new(2)
+      expect(new_conversion.convert_to_dec).to eq(10)
+    end
+
+    it "will return 11 for the number 3" do
+      new_conversion = Converter.new(3)
+      expect(new_conversion.convert_to_dec).to eq(11)
+    end
+
+    it "will return 11 for the number 3" do
+      new_conversion = Converter.new(156)
+      expect(new_conversion.convert_to_dec).to eq(10011100)
+    end
+  end
 end
